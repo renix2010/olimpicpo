d9047f3956c047ef8069f4b425c7674e08f86080
renix2010 <113396019+renix2010@users.noreply.github.com>
2022-12-22 17:42:02 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+//definir las variables para el canvas
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ color = "red"; 
+// Activamos el lapiz en la fila 7
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+// funcion para dibujar un circulo pide pocision x izquierda y derecha y arriba y abajo
+// pide radio y angulo de inicio y final
+ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+    //obtener color del cuadro de entrada
+    //inicio de la actividad adicional
+    color = document.getElementById("color").value;
+    console.log(color);
+    //final de la actividad adicional
+    
+     mouse_x = e.clientX - canvas.offsetLeft;
+     mouse_y = e.clientY - canvas.offsetTop;
+
+    console.log("X = " + mouse_x + " ,Y =  " + mouse_y);
+    circle(mouse_x , mouse_y);    
+}
+
+function circle(mouse_x , mouse_y)
+{
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+}
+
+//actividad adicional
+
+function clearArea()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+	
